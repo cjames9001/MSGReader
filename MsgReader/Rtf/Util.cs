893951bf46abccfd690920c835daee1d217e9508
Corey Pon <corey.pon@svault.int>
2018-08-13 16:33:35 -0500

    Merge pull request #112 from altso/net_core_support

--- MsgReader/Rtf/Util.cs
+++ MsgReader/Rtf/Util.cs
@@ -31,26 +31,5 @@ namespace MsgReader.Rtf
             return true;
         }
         #endregion
-        
-        #region Enum EmfToWmfBitsFlags
-        private enum EmfToWmfBitsFlags
-        {
-            // ReSharper disable UnusedMember.Local
-            // Use the default conversion
-            EmfToWmfBitsFlagsDefault = 0x00000000,
-
-            // Embedded the source of the EMF metafiel within the resulting WMF
-            // metafile
-            EmfToWmfBitsFlagsEmbedEmf = 0x00000001,
-
-            // Place a 22-byte header in the resulting WMF file.  The header is
-            // required for the metafile to be considered placeable.
-            EmfToWmfBitsFlagsIncludePlaceable = 0x00000002,
-
-            // Don't simulate clipping by using the XOR operator.
-            EmfToWmfBitsFlagsNoXorClip = 0x00000004
-            // ReSharper restore UnusedMember.Local
-        };
-        #endregion
     }
 }
